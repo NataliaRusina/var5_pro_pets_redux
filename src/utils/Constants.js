71a678df8855c6f67a1a0bdf618c81f5af011584
NataliaRusina <67218716+NataliaRusina@users.noreply.github.com>
2020-11-24 21:49:38 +0200

    last renew 24/11

---
+++ src/utils/Constants.js
@@ -0,0 +1,49 @@
+export const found_head = 'Found a pet? Please complete the form to help.'
+export const lost_head = 'Lost your buddy? Keep calm and complete the form.'
+export const preview_head = 'Preview and Publish. Please share the post to your FB to be more effective.'
+export const your_profile_head = 'Your profile. Change, edit and manage your data.'
+export const hotels_head = 'Hotels. Go to vacations — we’ll take care of your pet!'
+export const walking_head = 'Walking. No time tonight? We have a solution!'
+export const fostering_head = 'Fostering. In adoption we trust.'
+export const vet_help_head = 'VetHelp. They deserve it.'
+export const favorites_head = 'Your favorites. Find them here anytime.'
+export const new_post = 'Your new post! Simply text, add photos and publish.'
+
+export const base_url = 'https://propets-app.herokuapp.com';
+
+
+
+export const bigText = 'ever since the 1500s, when an unknown printer took a\n' +
+    '                    galley\n' +
+    '                    of type and\n' +
+    '                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the\n' +
+    '                    leap\n' +
+    '                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with\n' +
+    '                    the\n' +
+    '                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop\n' +
+    '                    publishing\n' +
+    '                    software like Aldus PageMaker including versions of Lorem Ipsum.\n' +
+    '                    <br/>\n' +
+    '                    Why do we use it? It is a long\n' +
+    '                    established fact that a reader will be distracted by the readable content of a page when looking at\n' +
+    '                    its\n' +
+    '                    layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,\n' +
+    '                    as\n' +
+    '                    opposed to using ‘Content here, content here’, making it look like readable English. Many desktop\n' +
+    '                    publishing packages and web page editors now use Lorem Ipsum as their default model text, and a\n' +
+    '                    search\n' +
+    '                    for ‘lorem ipsum’ will uncover many web sites still in their infancy. Various versions have evolved\n' +
+    '                    over\n' +
+    '                    the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n' +
+    '                    <br/>\n' +
+    '                    Contrary to\n' +
+    '                    popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin\n' +
+    '                    literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at\n' +
+    '                    Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from\n' +
+    '                    a\n' +
+    '                    Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the\n' +
+    '                    undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et\n' +
+    '                    Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the\n' +
+    '                    theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum\n' +
+    '                    dolor\n' +
+    '                    sit amet..”, comes from a line in section 1.10.32. The standard chunk of Lorem'
